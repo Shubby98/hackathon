9e96cf5c3e24e38da0a0b8c7ea34f53c9eaf5818
Shubby98 <40021444+Shubby98@users.noreply.github.com>
2018-10-21 04:40:39 +0530

    version 1.1

---
+++ server.py
@@ -0,0 +1,32 @@
+a=10
+while a>0:
+    import socket			 
+
+    # next create a socket object
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)		 
+    print ("Socket successfully created")
+
+    port=3000
+      
+    s.bind(("0.0.0.0", port))		 
+    print ("socket binded to %s" %(port))
+
+        # put the socket into listening mode 
+    s.listen(5)	 
+    print ("socket is listening")		
+
+            
+    c, addr = s.accept()	 
+    print ('Got connection from', addr )
+            
+            
+    #t='done done'
+            
+    #c.send(t.encode())
+
+    print(c.recv(1024))
+    a=a-1
+
+    s.close()      
+
+         
